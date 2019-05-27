00ef3220c04c5e189dcfbf06617047c685e325b9
Daniel Barclay <daniel@urbanintel.io>
2019-05-27 19:04:44 +0100

    Fetch event handler. Add To Homescreen banner visible

--- sw.js
+++ sw.js
@@ -2,3 +2,13 @@
 self.addEventListener("install", event => {
   console.log("service worker installed", event);
 });
+
+// activate service worker
+self.addEventListener("activate", event => {
+  console.log("service worker has been activated");
+});
+
+// fetch event
+self.addEventListener("fetch", event => {
+  console.log("fetch event", event);
+});
