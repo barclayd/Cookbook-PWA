478d8875a47685a351b7a65263c9caec18efdcce
Daniel Barclay <barclaysd@me.com>
2019-05-27 20:58:54 +0100

    Installed service works pre-caches resources. Fetch first checks cache before making server requests

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/materialize.min.css",
+  "/css/style.css",
+  "/img/dish.png",
+  "/index.html",
+  "/pages/about.html",
+  "/pages/contact.html",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+];
+
 // listen to service worker install event
-self.addEventListener("install", event => {
-  console.log("service worker installed", event);
+self.addEventListener("install", async event => {
+  // cache shell of pwa
+  event.waitUntil(
+    (cache = await caches.open(staticCacheName)),
+    console.log("caching shell assets"),
+    cache.addAll(assets)
+  );
 });
 
 // activate service worker
@@ -9,6 +30,7 @@ self.addEventListener("activate", event => {
 });
 
 // fetch event
-self.addEventListener("fetch", event => {
-  console.log("fetch event", event);
+self.addEventListener("fetch", async event => {
+  const { request } = event;
+  return event.respondWith(caches.match(request)) || fetch(request);
 });
