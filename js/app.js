6cbb9da487339a48006b88690b03ff4b0357da34
Daniel Barclay <daniel@urbanintel.io>
2019-05-27 16:04:50 +0100

    Registered and install service worker

---
+++ js/app.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("../sw.js")
+    .then(reg => {
+      console.log("registered service worker", reg);
+    })
+    .catch(err => {
+      console.log("service worker not registered", err);
+    });
+}
---
+++ sw.js
@@ -0,0 +1,4 @@
+// listen to service worker install event
+self.addEventListener("install", event => {
+  console.log("service worker installed", event);
+});
