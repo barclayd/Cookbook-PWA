394dbfff8356bff7d935df296b1b26f743454f4c
Daniel Barclay <daniel@urbanintel.io>
2019-05-31 10:45:40 +0100

    Replaced static data with output of real time data

--- js/db.js
+++ js/db.js
@@ -2,9 +2,10 @@ const collectionName = "recipes";
 // real time listener
 db.collection(collectionName).onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
-    // console.log(change, change.doc.data(), change.doc.id);
+    console.log(change, change.doc.data(), change.doc.id);
     if (change.type === "added") {
       // add document data to web page
+      renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === "removed") {
       // remove document data from web page
