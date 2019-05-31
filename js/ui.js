4940dde2b6890def59c39fd7dbde5ab8c389fa71
Daniel Barclay <barclaysd@me.com>
2019-05-31 10:45:40 +0100

    Replaced static data with output of real time data

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,5 @@
+const recipes = document.querySelector(".recipes");
+
 document.addEventListener("DOMContentLoaded", () => {
   // nav menu
   const menus = document.querySelector(".side-menu");
@@ -10,3 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
     edge: "right"
   });
 });
+
+const renderRecipe = (data, id) => {
+  const html = `
+  <div class="card-panel recipe white row" data-id=${id}>
+            <img src="/img/dish.png" alt="recipe thumb">
+            <div class="recipe-details">
+                <div class="recipe-title">${data.title}</div>
+                <div class="recipe-ingredients">${data.ingredients}</div>
+            </div>
+            <div class="recipe-delete">
+                <i class="material-icons" data-id=${id}>delete_outline</i>
+            </div>
+        </div>
+  `;
+  recipes.innerHTML += html;
+};
