6e3008cc792775d8cb54a3acc834d247593e442b
Daniel Barclay <barclaysd@me.com>
2019-06-09 18:14:44 +0100

    Tidied code and fixed waitUntil bug

--- js/db.js
+++ js/db.js
@@ -1,23 +1,23 @@
-// offline funcitonality
+// offline functionality
 db.enablePersistence().catch(err => {
   console.log(err);
-  if (err.code === "failed.precondition") {
-    console.log("persistent data failure");
-  } else if (err.code === "unimplemented") {
-    console.log("persistence is not available");
+  if (err.code === 'failed.precondition') {
+    console.log('persistent data failure');
+  } else if (err.code === 'unimplemented') {
+    console.log('persistence is not available');
   }
 });
 
-const collectionName = "recipes";
+const collectionName = 'recipes';
 // real time listener
 db.collection(collectionName).onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
     console.log(change, change.doc.data(), change.doc.id);
-    if (change.type === "added") {
+    if (change.type === 'added') {
       // add document data to web page
       renderRecipe(change.doc.data(), change.doc.id);
     }
-    if (change.type === "removed") {
+    if (change.type === 'removed') {
       // remove document data from web page
       removeRecipe(change.doc.id);
     }
@@ -26,31 +26,31 @@ db.collection(collectionName).onSnapshot(snapshot => {
 
 // add new recipe
 
-const form = document.querySelector("form");
-form.addEventListener("submit", async event => {
+const form = document.querySelector('form');
+form.addEventListener('submit', async event => {
   event.preventDefault();
 
   const recipe = {
     title: form.title.value,
-    ingredients: form.ingredients.value
+    ingredients: form.ingredients.value,
   };
 
   const data = await db.collection(collectionName).add(recipe);
   if (!data) {
-    throw new Error("Failed to add new recipe");
+    throw new Error('Failed to add new recipe');
   }
-  form.title.value = "";
-  form.ingredients.value = "";
+  form.title.value = '';
+  form.ingredients.value = '';
 });
 
 // delete a recipe
 
-const recipeContainer = document.querySelector(".recipes");
-recipeContainer.addEventListener("click", event => {
+const recipeContainer = document.querySelector('.recipes');
+recipeContainer.addEventListener('click', event => {
   const { target } = event;
-  if (target.tagName === "I") {
-    const id = target.getAttribute("data-id");
-    db.collection("recipes")
+  if (target.tagName === 'I') {
+    const id = target.getAttribute('data-id');
+    db.collection('recipes')
       .doc(id)
       .delete();
   }
