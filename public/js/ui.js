41ec799f6409213b0a45cce01a778b2dd58fa1bb
Daniel Barclay <daniel@urbanintel.io>
2019-06-09 18:36:56 +0100

    Fixed styling in header, errors with routing to different static pages and deployed to Firebase

--- public/js/ui.js
+++ public/js/ui.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const renderRecipe = (data, id) => {
   const html = `
   <div class="card-panel recipe white row" data-id=${id} id=${id}>
-            <img src="/img/dish.png" alt="recipe thumb">
+            <img src="../img/dish.png" alt="recipe thumb">
             <div class="recipe-details">
                 <div class="recipe-title">${data.title}</div>
                 <div class="recipe-ingredients">${data.ingredients}</div>
